bbfa6334bce4ed96d4e3c9616334544fb18646b7
Zack Winchell <zackwinchell@gmail.com>
2016-03-31 12:04:09 -0700

    moves posts in league endpoint from /post/:leagueId to /league/:leagueId/posts

--- routes/api/league/index.js
+++ routes/api/league/index.js
@@ -4,8 +4,15 @@
   const router = express.Router();
 
   const League = require(global.models + '/League');
+  const Post = require(global.models + '/Post');
   const User = require(global.models + '/User');
 
+  router.get('/:leagueId/posts', User.isLoggedIn, (req, res) => {
+    Post.find({ league: req.params.leagueId }).populate('author').exec((err, posts) => {
+      if (err) { return res.status(400).send(err); }
+      res.send(posts);
+    });
+  });
 
   router.get('/:leagueId', User.isLoggedIn, League.detailsMW, (req, res) => {
     res.send(req.details);
