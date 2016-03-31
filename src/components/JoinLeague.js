a321d50c7a81662ca27a6b2b4bbb800663857d3d
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-30 19:15:31 -0700

    style join league

--- src/components/JoinLeague.js
+++ src/components/JoinLeague.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { reduxForm } from 'redux-form';
 import { joinLeague, verifyLogin } from '../actions/index';
 // import Cookies from 'cookies-js';
@@ -21,39 +22,42 @@ class JoinLeague extends Component {
     const { fields: { leagueId, team }, handleSubmit } = this.props;
 
     return (
-      <div className="join-league-form">
-        <h2 className="login-title">Join League</h2>
-
-        <form
-          onSubmit={handleSubmit(this.onSubmit.bind(this))}
-          className="col-xs-10 col-xs-offset-1">
-          <div className={`form-group ${leagueId.touched && leagueId.invalid ? 'has-danger' : ''}`}>
-            <label>League Id</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter League Id"
-              {...leagueId}
-              />
-              <div className="text-help">
-                {leagueId.touched ? leagueId.error : ''}
-              </div>
-          </div>
-          <div className={`form-group ${team.touched && team.invalid ? 'has-danger' : ''}`}>
-            <label>Team Name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Team Name"
-              {...team}
-              />
-              <div className="text-help">
-                {team.touched ? team.error : ''}
-              </div>
-          </div>
-          <button type="submit" className="btn btn-default">Join</button>
-        </form>
-
+      <div className="login-register-form">
+        <h3>Join an existing league.</h3>
+        <h4 className="login-title">Join with the league ID.</h4>
+        <div className="form-wrapper col-xs-6 col-xs-offset-3">
+          <img src="../images/ajax_get_colored_svg2.png" width="35%" alt=""/>
+          <form
+            onSubmit={handleSubmit(this.onSubmit.bind(this))}
+            className="col-xs-10 col-xs-offset-1">
+            <div className={`form-group ${leagueId.touched && leagueId.invalid ? 'has-danger' : ''}`}>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter the league ID"
+                {...leagueId}
+                />
+                <div className="text-help">
+                  {leagueId.touched ? leagueId.error : ''}
+                </div>
+            </div>
+            <div className={`form-group ${team.touched && team.invalid ? 'has-danger' : ''}`}>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your team name"
+                {...team}
+                />
+                <div className="text-help">
+                  {team.touched ? team.error : ''}
+                </div>
+            </div>
+            <button type="submit" className="btn btn-success form-control">Join</button>
+          </form>
+        </div>
+        <div className="col-xs-6 col-xs-offset-3">
+          <Link to="/create" className="login-noaccount" href="#">I want to create a league</Link>
+        </div>
       </div>
     );
   }
