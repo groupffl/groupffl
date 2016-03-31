4a1a2c507ed41368b281162de6a4994f5655b537
valmsmith39a <valmsmith39a@gmail.com>
2016-03-31 08:39:30 -0700

    Cleans up code in LeaguePosts

--- src/components/LeaguePosts.js
+++ src/components/LeaguePosts.js
@@ -3,31 +3,28 @@ import { connect } from 'react-redux';
 import { createPost, fetchPosts } from '../actions/index';
 
 class LeaguePosts extends Component {
-  constructor(props) {
-    super(props);
-    console.log('props', props);
+
+  componentWillMount() {
+    this.props.fetchPosts(this.props.params.id)
+      .then(() => {
+      });
   }
 
   addPost() {
-    console.log('post is: ', this.refs.postInput.value);
-    console.log('params is: ', this.props.params.leagueId);
     const postObj = {
       description: this.refs.postInput.value,
       leagueId: this.props.params.id,
       title: 'NA'
     };
     this.props.createPost(postObj)
-      .then(res => {
-        console.log('got posts', res);
-        this.props.fetchPosts(this.props.params.id)
-          .then(res => {
-            console.log('all posts', res);
-          });
-      });
+    .then(() => {
+      this.props.fetchPosts(this.props.params.id)
+        .then(() => {
+        });
+    });
   }
 
   renderList() {
-    console.log(this.props.all)
     return this.props.all.map(post => {
       return (
         <li>{post.description}</li>
@@ -36,11 +33,10 @@ class LeaguePosts extends Component {
   }
 
   render() {
-    console.log(this.props.leaguePosts);
     return (
       <div className="posts">
         <div className="post-wrapper">
-          <textarea ref="postInput" type="text" className="post-area" placeholder="hello?"/>
+          <textarea ref="postInput" type="text" className="post-area" placeholder=""/>
           <button onClick={this.addPost.bind(this)} className="btn btn-primary pull-right">POST</button>
           <div>
             <ul>
