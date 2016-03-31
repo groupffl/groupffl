e4b9b9be6f8231cb31246bd95f7d4aff213bf3e2
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-31 16:24:19 -0700

    creates textarea and comment button

--- src/components/PostsComments.js
+++ src/components/PostsComments.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 class PostsComments extends Component {
@@ -8,11 +7,21 @@ class PostsComments extends Component {
     console.log(props);
   }
 
+  createComment() {
+    console.log(this.refs.commentInput.value);
+  }
+
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
-      <div>
-        This is a comment
+      <div className="comments">
+        <div className="comment-area">
+          <textarea ref="commentInput" className="comment-area" />
+          <button
+            onClick={this.createComment.bind(this)}
+            className="btn btn-success pull-right">Comment
+          </button>
+        </div>
       </div>
     );
   }
