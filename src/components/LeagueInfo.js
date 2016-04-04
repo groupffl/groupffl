6a64229fc21af4747b4f7f17ce5874cc327ce625
Zack Winchell <zackwinchell@gmail.com>
2016-04-04 13:31:40 -0700

    adds mailto, makes links open in new tab/window

--- src/components/LeagueInfo.js
+++ src/components/LeagueInfo.js
@@ -3,14 +3,9 @@ import { connect } from 'react-redux';
 import { fetchLeagueInfo } from '../actions/index';
 
 class LeagueInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentWillMount() {
     this.props.fetchLeagueInfo(this.props.leagueId)
-      .then(() => {
-      });
+      .then(() => {});
   }
 
   render() {
@@ -21,23 +16,24 @@ class LeagueInfo extends Component {
     }
 
     const { leagueInfo } = this.props;
+    const mailto = `mailto:?to=&subject=Join%20My%20League%20On%20GFFL!&body=localhost:3000/join%0D%0Aleague%20ID:%20${leagueInfo._id}`; // FIXME: Replace localhost with deployed URL
 
     return (
       <div>
         <h3>{leagueInfo.name}</h3>
         <div className="league-info-details">
           <h4>League ID</h4>
-          <p>{leagueInfo._id}</p>
+          <a href={mailto} target="_blank">{leagueInfo._id}</a>
           <h4>FFL URL</h4>
           <a href="#">{leagueInfo.fflUrl}</a>
           <h4>Commissioner</h4>
           <p>Team Name:<br />{leagueInfo.commissionerTeamName}</p>
           <p>Email:<br />{leagueInfo.commissioner.email}</p>
           <h4>Related Links</h4>
-          <a className="related-links" href="http://games.espn.go.com/frontpage/football">ESPN Fantasy News</a>
-          <a className="related-links" href="http://football.fantasysports.yahoo.com/">Yahoo Fantasy News</a>
-          <a className="related-links" href="http://www.nfl.com/fantasyfootball/playernews">NFL Fantasy News</a>
-          <a className="related-links" href="http://www.rotoworld.com/playernews/nfl/football/">Rotoworld Fantasy News</a>
+          <a className="related-links" href="http://games.espn.go.com/frontpage/football" target="_blank">ESPN Fantasy News</a>
+          <a className="related-links" href="http://football.fantasysports.yahoo.com/" target="_blank">Yahoo Fantasy News</a>
+          <a className="related-links" href="http://www.nfl.com/fantasyfootball/playernews" target="_blank">NFL Fantasy News</a>
+          <a className="related-links" href="http://www.rotoworld.com/playernews/nfl/football/" target="_blank">Rotoworld Fantasy News</a>
         </div>
       </div>
     );
