2c77c3267b0eaf3c94773efb54feb85618a4199e
jtabach <jeff.n.tabachnick@gmail.com>
2016-07-09 12:52:04 -0700

    add(api/feed) caching layer for render speed

--- routes/api/feed/index.js
+++ routes/api/feed/index.js
@@ -6,14 +6,35 @@
   const request = require('request');
   const RSS_URL = 'https://api.fantasydata.net/nfl/v2/json/News';
 
+  let cache = {};
+
+  // Initiate Api Call
+  getApiReponse();
+
+  // Get new live updates every 6 hours
+  // Limit 1000 uses per API Key
+  setInterval(function() {
+    getApiReponse();
+  }, 21600000);
+
+  // Return cached response
   router.get('/rss', (req, res) => {
+    if (cache) {
+      return res.send(cache);
+    } else {
+      getApiReponse();
+    }
+  });
+
+  // Make Api Call
+  function getApiReponse() {
     request(RSS_URL, {
       headers: { 'Ocp-Apim-Subscription-Key': process.env.FANTASYDATA_API_KEY }
     }, function(err, response) {
-      if (err) { return res.status(400).send(err); }
-      res.send(response.body);
+      if (err) { return console.log(err); }
+      cache = response.body;
     });
-  });
+  }
 
   module.exports = router;
 }());
