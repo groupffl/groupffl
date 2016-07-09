a0a9c4b89ae5a2733363addd0a94d5c4bbe24aef
jtabach <jeff.n.tabachnick@gmail.com>
2016-07-09 10:00:36 -0700

    fix(CORS) revert server.js

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ const chalk = require('chalk');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
-const Helmet = require('react-helmet');
 
 global.models = path.join(__dirname, '/models/');
 
 const app = express();
 
 const mongoose = require('mongoose');
-const mongoUrl = process.env.MLAB_URI || 'mongodb://localhost/fflnm';
+const mongoUrl = process.env.MLAB_URI || 'mongodb://localhost/ffln';
 
 const mongoConnectMsg = process.env.MLAB_URI ? '.' : chalk.cyan(` ${mongoUrl}`);
 
@@ -35,35 +34,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', require('./routes/api'));
-
-
-
-app.get('/*', (req, res) => {
-  var head = Helmet.rewind();
-  const html = `
-  <!DOCTYPE html>
-  <html ${head.htmlAttributes.toString()} lang="en">
-  <head>
-    ${head.title.toString()}
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="google-site-verification" content="cMlN8L9GeFFsaAT5zymDbxz5vl-ROjhp5s6ydhBhVRw" />
-    <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="shortcut icon" href="http://i.imgur.com/AgquJCe.gif" type="image/x-icon">
-    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
-  </head>
-  <body>
-    <div id="app"></div>
-    <script src="/bundle.js"></script>
-  </body>
-  </html>
-  `;
-  res.write(html);
-  res.end();
+app.use('/*', (req, res) => {
+  res.render('index');
 });
 
 app.use((req, res, next) => {
