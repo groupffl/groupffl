7336892f800213aeb2fe9be42bd9096dbd420522
jtabach <jeff.n.tabachnick@gmail.com>
2016-07-09 17:48:04 -0700

    improve(RelatedLinks) move data out of view layer

---
+++ data/relatedLinks.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    url: 'http://games.espn.go.com/frontpage/football',
+    title: 'ESPN Fantasy News'
+  },
+  {
+    url: 'http://football.fantasysports.yahoo.com',
+    title: 'Yahoo Fantasy News'
+  },
+  {
+    url: 'http://www.nfl.com/fantasyfootball',
+    title: 'NFL Fantasy News'
+  },
+  {
+    url: 'http://www.rotoworld.com/playernews/nfl/football',
+    title: 'Rotoworld Fantasy News'
+  }
+];
--- src/components/League/Info/RelatedLinks.js
+++ src/components/League/Info/RelatedLinks.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.scss';
-
-const ESPN_FANTASY_URL = 'http://games.espn.go.com/frontpage/football';
-const YAHOO_FANTASY_URL = 'http://football.fantasysports.yahoo.com';
-const NFL_FANTASY_URL = 'http://www.nfl.com/fantasyfootball';
-const ROTO_FANTASY_URL = 'http://www.rotoworld.com/playernews/nfl/football';
+import relatedLinks from '../../../../data/relatedLinks.js';
 
 function RelatedLinks() {
   return (
     <div>
       <h4>Related Links</h4>
-      <a styleName="related-links" href={ESPN_FANTASY_URL} target="_blank">ESPN Fantasy News</a>
-      <a styleName="related-links" href={YAHOO_FANTASY_URL} target="_blank">Yahoo Fantasy News</a>
-      <a styleName="related-links" href={NFL_FANTASY_URL} target="_blank">NFL Fantasy News</a>
-      <a styleName="related-links" href={ROTO_FANTASY_URL} target="_blank">Rotoworld Fantasy News</a>
+      {
+        relatedLinks.map((link, i) => {
+          return (
+            <a
+              styleName="related-links"
+              href={link.url}
+              target="_blank"
+              key={i}>
+              {link.title}
+            </a>
+          );
+        })
+      }
     </div>
   );
 }
